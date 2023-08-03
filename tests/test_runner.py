7983234ab3df9613f4736cbd5e7ed416c627497a
Tomasz Pietruszka <tomek.pietruszka@gmail.com>
2023-08-03 20:52:05 +0100

    Added tests for the estimation mechanism

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -17,13 +17,17 @@ def dummy_client(url: str, how_many: int, failure_proba: float = 0.0) -> dict:
     return parsed
 
 
-def dummy_resources(with_estimation: bool = True) -> list[Resource]:
+def dummy_resources(
+    num_requests: int = 3, num_points: int = 20, with_estimation: bool = True
+) -> list[Resource]:
     estimator = (lambda call: 2 * call.get_argument("how_many")) if with_estimation else None
     return [
-        Resource("requests", 3, time_window_seconds=5, arguments_usage_extractor=lambda _: 1),
+        Resource(
+            "requests", num_requests, time_window_seconds=5, arguments_usage_extractor=lambda _: 1
+        ),
         Resource(
             name="points",
-            quota=20,
+            quota=num_points,
             time_window_seconds=5,
             results_usage_extractor=lambda x: x["used_points"],
             max_results_usage_estimator=estimator,
@@ -119,3 +123,66 @@ def test_refuse_too_large_task(running_dummy_server):
     runner = get_runner(dummy_resources())
     with pytest.raises(ValueError, match="exceeds resource quota "):
         runner.schedule(running_dummy_server, 1000)
+
+
+def test_runner_without_estimation(running_dummy_server):
+    """
+    Runner with estimation - temporarily exceeds the points quota.
+
+    The dummy server only registers usage when the results are returned,
+    which allows more tasks to start in parallel than the points quota allows.
+
+    This is to be contrasted with the similar test below, which uses the same resources, but
+    with estimation enabled.
+    """
+    num_requests = 10
+    # setting the number of points to be equal to the number of requests,
+    # so points quota actually only lets through half of the requests at a time
+    runner = get_runner(
+        dummy_resources(num_requests=num_requests, num_points=num_requests, with_estimation=False)
+    )
+
+    for _ in range(num_requests):
+        runner.schedule(running_dummy_server, 1)
+
+    results, exceptions = runner.run()
+
+    outputs = [result["output"] for result in results]
+    assert outputs == ["x"] * num_requests
+    assert exceptions == [[]] * num_requests
+
+    # check that the points quota was not exceeded
+    points_used = [
+        result["used_points"] + result["state_before_check"]["points"] for result in results
+    ]
+    assert max(points_used) == num_requests * 2
+
+
+def test_runner_with_estimation(running_dummy_server):
+    """
+    Runner with estimation - does not exceed the points quota.
+
+    The dummy server only registers usage when the results are returned, but others would kill
+    requests mid-way. We wan't to make sure this won't happen in this scenario
+    """
+    num_requests = 10
+    # setting the number of points to be equal to the number of requests,
+    # so points quota actually only lets through half of the requests at a time
+    runner = get_runner(
+        dummy_resources(num_requests=num_requests, num_points=num_requests, with_estimation=True)
+    )
+
+    for _ in range(num_requests):
+        runner.schedule(running_dummy_server, 1)
+
+    results, exceptions = runner.run()
+
+    outputs = [result["output"] for result in results]
+    assert outputs == ["x"] * num_requests
+    assert exceptions == [[]] * num_requests
+
+    # check that the points quota was not exceeded
+    points_used = [
+        result["used_points"] + result["state_before_check"]["points"] for result in results
+    ]
+    assert max(points_used) <= num_requests
